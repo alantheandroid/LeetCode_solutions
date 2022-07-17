58d70733e72f5741a037971ffe38e84dbce4c670
Alessio Ilardi <alessio.ilardi@gmail.com>
2022-07-17 19:16:14 +0200

    first commit

---
+++ 01_two_sum/index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const previousValues = {};
+  for (let i = 0; i < nums.length; i++) {
+    const currentNumber = nums[i];
+    const neededValue = target - currentNumber;
+    const index2 = previousValues[neededValue];
+    if (index2 != null) {
+      return [index2, i];
+    } else {
+      previousValues[currentNumber] = i;
+    }
+  }
+};
+
+/* const inputNums = [2, 7, 11, 15];
+const targetSum = 9;
+
+console.log(twoSum(inputNums, targetSum));
+ */
---
+++ 01_two_sum/readme.md
@@ -0,0 +1,1 @@
+![problem description](/leetcode.com_problems_two-sum_.png)
